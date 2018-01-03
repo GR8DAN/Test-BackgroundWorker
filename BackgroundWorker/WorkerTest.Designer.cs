b4cd113ce762525c9290943562518a3186ea8d7d
Daniel S. Fowler <danfowler@sky.com>
2018-01-03 12:40:09 +0000

    Initial working version
    Initial working version of a simple program to test BackgroundWorker. The program processes a file on another thread and counts the frequency of english letters it contains.

---
+++ BackgroundWorker/WorkerTest.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WorkerTest
+{
+    partial class WorkerTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LstStatus = new System.Windows.Forms.ListBox();
+            this.BgrdWorker = new System.ComponentModel.BackgroundWorker();
+            this.Progress = new System.Windows.Forms.ProgressBar();
+            this.FileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.ButCancel = new System.Windows.Forms.Button();
+            this.ButGo = new System.Windows.Forms.Button();
+            this.TxtFile = new System.Windows.Forms.TextBox();
+            this.ButChooseFile = new System.Windows.Forms.Button();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.LblPercent = new System.Windows.Forms.Label();
+            this.ButClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LstStatus
+            // 
+            this.LstStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LstStatus.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LstStatus.FormattingEnabled = true;
+            this.LstStatus.ItemHeight = 20;
+            this.LstStatus.Location = new System.Drawing.Point(14, 92);
+            this.LstStatus.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.LstStatus.Name = "LstStatus";
+            this.LstStatus.Size = new System.Drawing.Size(588, 244);
+            this.LstStatus.TabIndex = 0;
+            // 
+            // BgrdWorker
+            // 
+            this.BgrdWorker.WorkerReportsProgress = true;
+            this.BgrdWorker.WorkerSupportsCancellation = true;
+            this.BgrdWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgrdWorker_DoWork);
+            this.BgrdWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BgrdWorker_ProgressChanged);
+            this.BgrdWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgrdWorker_RunWorkerCompleted);
+            // 
+            // Progress
+            // 
+            this.Progress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Progress.Location = new System.Drawing.Point(68, 63);
+            this.Progress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Progress.Name = "Progress";
+            this.Progress.Size = new System.Drawing.Size(536, 20);
+            this.Progress.TabIndex = 1;
+            // 
+            // FileDialog
+            // 
+            this.FileDialog.Filter = "Text files|*.txt|All files|*.*";
+            // 
+            // ButCancel
+            // 
+            this.ButCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButCancel.Enabled = false;
+            this.ButCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButCancel.Location = new System.Drawing.Point(558, 15);
+            this.ButCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ButCancel.Name = "ButCancel";
+            this.ButCancel.Size = new System.Drawing.Size(45, 35);
+            this.ButCancel.TabIndex = 11;
+            this.ButCancel.Text = "[]";
+            this.ButCancel.UseVisualStyleBackColor = true;
+            this.ButCancel.Click += new System.EventHandler(this.ButCancel_Click);
+            // 
+            // ButGo
+            // 
+            this.ButGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButGo.Enabled = false;
+            this.ButGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButGo.Location = new System.Drawing.Point(504, 15);
+            this.ButGo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ButGo.Name = "ButGo";
+            this.ButGo.Size = new System.Drawing.Size(45, 35);
+            this.ButGo.TabIndex = 10;
+            this.ButGo.Text = ">";
+            this.ButGo.UseVisualStyleBackColor = true;
+            this.ButGo.Click += new System.EventHandler(this.ButGo_Click);
+            // 
+            // TxtFile
+            // 
+            this.TxtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtFile.Location = new System.Drawing.Point(68, 18);
+            this.TxtFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TxtFile.Name = "TxtFile";
+            this.TxtFile.Size = new System.Drawing.Size(372, 26);
+            this.TxtFile.TabIndex = 9;
+            // 
+            // ButChooseFile
+            // 
+            this.ButChooseFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButChooseFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButChooseFile.Location = new System.Drawing.Point(450, 15);
+            this.ButChooseFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ButChooseFile.Name = "ButChooseFile";
+            this.ButChooseFile.Size = new System.Drawing.Size(45, 35);
+            this.ButChooseFile.TabIndex = 8;
+            this.ButChooseFile.Text = "...";
+            this.ButChooseFile.UseVisualStyleBackColor = true;
+            this.ButChooseFile.Click += new System.EventHandler(this.ButChooseFile_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(9, 23);
+            this.lblFile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(38, 20);
+            this.lblFile.TabIndex = 7;
+            this.lblFile.Text = "File:";
+            // 
+            // LblPercent
+            // 
+            this.LblPercent.AutoSize = true;
+            this.LblPercent.Location = new System.Drawing.Point(9, 63);
+            this.LblPercent.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblPercent.Name = "LblPercent";
+            this.LblPercent.Size = new System.Drawing.Size(50, 20);
+            this.LblPercent.TabIndex = 12;
+            this.LblPercent.Text = "100%";
+            // 
+            // ButClear
+            // 
+            this.ButClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ButClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButClear.Location = new System.Drawing.Point(14, 366);
+            this.ButClear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ButClear.Name = "ButClear";
+            this.ButClear.Size = new System.Drawing.Size(68, 35);
+            this.ButClear.TabIndex = 13;
+            this.ButClear.Text = "Clear";
+            this.ButClear.UseVisualStyleBackColor = true;
+            this.ButClear.Click += new System.EventHandler(this.ButClear_Click);
+            // 
+            // WorkerTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 411);
+            this.Controls.Add(this.ButClear);
+            this.Controls.Add(this.LblPercent);
+            this.Controls.Add(this.ButCancel);
+            this.Controls.Add(this.ButGo);
+            this.Controls.Add(this.TxtFile);
+            this.Controls.Add(this.ButChooseFile);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.Progress);
+            this.Controls.Add(this.LstStatus);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimumSize = new System.Drawing.Size(364, 354);
+            this.Name = "WorkerTest";
+            this.Text = "Background Worker Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LstStatus;
+        private System.ComponentModel.BackgroundWorker BgrdWorker;
+        private System.Windows.Forms.ProgressBar Progress;
+        private System.Windows.Forms.OpenFileDialog FileDialog;
+        private System.Windows.Forms.Button ButCancel;
+        private System.Windows.Forms.Button ButGo;
+        private System.Windows.Forms.TextBox TxtFile;
+        private System.Windows.Forms.Button ButChooseFile;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Label LblPercent;
+        private System.Windows.Forms.Button ButClear;
+    }
+}
+
---
+++ BackgroundWorker/WorkerTest.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WorkerTest
+{
+    public partial class WorkerTest : Form
+    {
+        long FileLength;        //Store the length of the file being processed
+        long BytesProcessed;    //Count the characters processed
+
+        //Count the frequency of different characters
+        long LowercaseEnglishLetter = 0;
+        long LowercaseNonEnglishLetter = 0;
+        long UppercaseEnglishLetter = 0;
+        long UppercaseNonEnglishLetter = 0;
+        long EnglishLetter = 0;
+        long Digit0to9 = 0;
+        long Whitespace = 0;
+        long ControlCharacter = 0;
+        //Count the frequency of English letters
+        long[] LetterFrequency=new long[26];
+
+        public WorkerTest()
+        {
+            InitializeComponent();
+        }
+
+        #region  BackgroundWorker
+        private void BgrdWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            int nextChar;
+ 
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(TxtFile.Text))
+                {
+                    while((nextChar = sr.Read()) != -1)
+                    {
+                        if (worker.CancellationPending == true)
+                        {
+                            e.Cancel = true;
+                            break;
+                        }
+                        else
+                        {
+                            //Now process the character
+                            AnalyseChar((char)nextChar);
+                            BytesProcessed += 1;
+                            //Report back every 100,000 chars
+                            if (BytesProcessed % 100000 == 0)
+                            {
+                                //report progress
+                                worker.ReportProgress(1, BytesProcessed);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // What was the problem
+                AddMessage(ex.Message);
+            }
+        }
+
+        private void BgrdWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            AddMessage("Processed: " + ((long)e.UserState).ToString() + " bytes");
+            Progress.Value = (int)Math.Ceiling(((float)BytesProcessed / FileLength) * 100);
+            LblPercent.Text = Progress.Value.ToString() + "%";
+        }
+
+        private void BgrdWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ButCancel.Enabled = false;
+            if (e.Cancelled == true)
+            {
+                AddMessage("Analysis aborted.");
+            }
+            else if (e.Error != null)
+            {
+                AddMessage("Analysis error: " + e.Error.Message);
+            }
+            else
+            {
+                AddMessage("Analysis completed, bytes processed: " + BytesProcessed.ToString());
+                Progress.Value = 100;
+                LblPercent.Text = "100%";
+
+                if (LowercaseEnglishLetter > 0)
+                    AddMessage("Total Lowercase English Letters=" + LowercaseEnglishLetter.ToString());
+                if (UppercaseEnglishLetter > 0)
+                    AddMessage("Total Uppercase English Letters=" + UppercaseEnglishLetter.ToString());
+                if (LowercaseNonEnglishLetter > 0)
+                    AddMessage("Total Lowercase Non-English Letters=" + LowercaseNonEnglishLetter.ToString());
+                if (UppercaseNonEnglishLetter > 0)
+                    AddMessage("Total Uppercase Non-english Letters=" + UppercaseNonEnglishLetter.ToString());
+                if (Digit0to9 > 0)
+                    AddMessage("Total Digits=" + Digit0to9.ToString());
+                if (Whitespace > 0)
+                    AddMessage("Total Whitespace=" + Whitespace.ToString());
+                if (ControlCharacter > 0)
+                    AddMessage("Total Control Characters=" + ControlCharacter.ToString());
+                AddMessage("");
+                if (EnglishLetter > 0) {
+                    AddMessage("Total number of English letters:" + EnglishLetter.ToString());
+                    double LetterPercentage;
+                    string PrintResult;
+                    for (int i = 0; i < 26; i++)
+                    {
+                        LetterPercentage= ((double)LetterFrequency[i] / EnglishLetter) * 100.0;
+                        PrintResult=((char)(i + 65)).ToString();
+                        for (int j = 0; j < Math.Round(LetterPercentage); j++)
+                            PrintResult += "-";
+                        AddMessage(PrintResult + " " + LetterPercentage.ToString("n3") + "%");
+                    }
+                }
+            }
+        }
+        #endregion
+        #region Buttons
+        private void ButChooseFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = FileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                TxtFile.Text = FileDialog.FileName;
+                ButGo.Enabled = true;
+            }
+        }
+
+        private void ButGo_Click(object sender, EventArgs e)
+        {
+            ButCancel.Enabled = true;
+            Analysis();
+        }
+
+        private void ButCancel_Click(object sender, EventArgs e)
+        {
+            if (BgrdWorker.WorkerSupportsCancellation == true)
+            {
+                // Cancel the asynchronous operation.
+                BgrdWorker.CancelAsync();
+            }
+        }
+
+        private void ButClear_Click(object sender, EventArgs e)
+        {
+            LstStatus.Items.Clear();
+            Progress.Value = 0;
+            FileLength = 0;
+            BytesProcessed = 0;
+            LetterFrequency = new long[26];
+            LowercaseEnglishLetter = 0;
+            LowercaseNonEnglishLetter = 0;
+            UppercaseEnglishLetter = 0;
+            UppercaseNonEnglishLetter = 0;
+            EnglishLetter = 0;
+            Digit0to9 = 0;
+            Whitespace = 0;
+            ControlCharacter = 0;
+        }
+        #endregion
+
+        //Analyse the text file
+        void Analysis()
+        {
+            //Clear previous results
+            ButClear_Click(this,null);
+            if (!File.Exists(TxtFile.Text))
+            {
+                AddMessage("The file " + TxtFile.Text + " does not exist.");
+            }
+            else
+            {
+                AddMessage("Starting letter analysis...");
+                if (BgrdWorker.IsBusy != true)
+                {
+                    FileLength = (new FileInfo(TxtFile.Text)).Length;
+                    // Start the asynchronous operation.
+                    BgrdWorker.RunWorkerAsync();
+                }
+                else
+                {
+                    AddMessage("Please wait.");
+                }
+            }
+        }
+        //Analyse a single character
+        void AnalyseChar(char Character)
+        {
+            if (char.IsLower(Character))
+            {
+                if (Character >= 'a' && Character <= 'z')
+                {
+                    LowercaseEnglishLetter++;
+                    LetterFrequency[Character - 'a']++;
+                }
+                else
+                {
+                    LowercaseNonEnglishLetter++;
+                }
+            }
+            else if(char.IsUpper(Character))
+            {
+                if (Character >= 'A' && Character <= 'Z')
+                {
+                    UppercaseEnglishLetter++;
+                    LetterFrequency[Character - 'A']++;
+                }
+                else
+                {
+                    UppercaseNonEnglishLetter++;
+                }
+            }
+            else if (char.IsDigit(Character))
+            {
+                Digit0to9++;
+            }
+            else if (char.IsWhiteSpace(Character))
+            {
+                Whitespace++;
+            }
+            else if (char.IsControl(Character))
+            {
+                ControlCharacter++;
+            }
+            EnglishLetter = LowercaseEnglishLetter + UppercaseEnglishLetter;
+        }
+        //User feedback
+        int AddMessage(string MessageToAdd)
+        {
+            //Limit number of items
+            if (LstStatus.Items.Count >= 60000)
+                LstStatus.Items.RemoveAt(0);
+            int ret = LstStatus.Items.Add(MessageToAdd);
+            //ensure new item is visible
+            LstStatus.TopIndex = LstStatus.Items.Count - 1;
+            return ret;
+        }
+    }
+}
